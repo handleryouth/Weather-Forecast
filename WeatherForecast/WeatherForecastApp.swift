7fd93f5904690d0686f2998a95c0196726d2361f
Tony David <70279376+handleryouth@users.noreply.github.com>
2024-08-13 08:36:41 +0700

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Tony Gultom on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
